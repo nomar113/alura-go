b9264cbd23117b84ba2dcc854db4610f51ee5a66
Ramon Mesquita <ramonmesquita113@gmail.com>
2025-05-09 12:17:32 -0300

    POST

--- main.go
+++ main.go
@@ -6,19 +6,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getPizzas(ctx *gin.Context) {
-	var pizzas = []models.Pizza{
-		{ID: 1, Name: "Toscana", Price: 49.5},
-		{ID: 2, Name: "Marguerita", Price: 79.5},
-		{ID: 3, Name: "Frango com requeijão", Price: 69.5},
-	}
-	ctx.JSON(200, gin.H{
-		"message": pizzas,
-	})
+var pizzas = []models.Pizza{
+	{ID: 1, Name: "Toscana", Price: 49.5},
+	{ID: 2, Name: "Marguerita", Price: 79.5},
+	{ID: 3, Name: "Frango com requeijão", Price: 69.5},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", postPizzas)
 	router.Run()
+}
+
+func getPizzas(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": pizzas,
+	})
+}
+
+func postPizzas(ctx *gin.Context) {
+	var newPizza models.Pizza
+	if err := ctx.ShouldBindJSON(&newPizza); err != nil {
+		ctx.JSON(400, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	pizzas = append(pizzas, newPizza)
 }
