25bbad383d649798ba8d5d30f7d8ef93ec56bde7
Ramon Mesquita <ramonmesquita113@gmail.com>
2025-05-09 06:03:37 -0300

    struct and list

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	ID    int
+	name  string
+	price float64
+}
+
 func main() {
-	var name = "Pizzaria Go"
-	instagram, telefone := "@pizzaria_go", 11919829405
-	fmt.Println(name, instagram, telefone)
+	var pizzas = []Pizza{
+		{ID: 1, name: "Toscana", price: 49.5},
+		{ID: 2, name: "Marguerita", price: 79.5},
+		{ID: 3, name: "Atum com queijo", price: 69.5},
+	}
+	fmt.Println(pizzas)
 }
